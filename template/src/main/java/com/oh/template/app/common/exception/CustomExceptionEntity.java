fcec8b1eac28de76242df08cd0fb40e1eb6ba612
ohju96 <ohju96@naver.com>
2024-01-24 17:49:57 +0900

    fix : 커스텀 예외처리 엔티티 수정 및 애플리케이션 어노테이션 추가

--- template/src/main/java/com/oh/template/app/common/exception/CustomExceptionEntity.java
+++ template/src/main/java/com/oh/template/app/common/exception/CustomExceptionEntity.java
@@ -1,13 +1,7 @@
 package com.oh.template.app.common.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
 
-@Getter
-@ToString
-@AllArgsConstructor
 @Builder
 public record CustomExceptionEntity(
         String result,
